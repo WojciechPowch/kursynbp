2bd25d8e2c1f74fe44fe1b2726cb4cdefb1fbc1a
Wojciech <powch16@gmail.com>
2018-07-10 21:55:39 +0200

    NbpParserEngine
    Dodano klasę parser dla api nbp

---
+++ src/main/java/nbpengine/NbpParserSn.java
@@ -0,0 +1,148 @@
+package nbpengine;
+
+import java.util.Map;
+import org.xml.sax.InputSource;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.*;
+import java.io.BufferedReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.*;
+import javax.xml.parsers.*;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+/**
+ * Dodać metodę pobierania wszystkich nazw walut które są obecnie dostępne
+ * */
+
+public class NbpParserSn {
+    private Map<String, String[]> courses;
+    private static NbpParserSn nbpParseSn = new NbpParserSn();
+
+    private NbpParserSn(){
+        initializeCourses();
+    }
+
+    static NbpParserSn getInstance(){
+        return nbpParseSn;
+    }
+
+    private String getUrlContent(String urlAddress)
+    {
+        URL url;
+        URLConnection urlConnection;
+        BufferedReader bufferedReader;
+        StringBuilder stringBuilder;
+        String ret = null;
+
+        try
+        {
+
+            url = new URL(urlAddress);
+            urlConnection = url.openConnection();
+            bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),StandardCharsets.UTF_8));
+            stringBuilder = new StringBuilder();
+
+            String helper = "";
+
+            while((helper=bufferedReader.readLine())!=null)
+            {
+                stringBuilder.append(helper);
+            }
+
+            ret = stringBuilder.toString();
+
+        }catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return ret;
+    }
+
+    private String getNode(Node node, String tagname)
+    {
+        Element elemet = (Element) node;
+        NodeList firstNodeList = elemet.getElementsByTagName(tagname);
+        Element midElement = (Element) firstNodeList.item(0);
+        NodeList secondNodeList = midElement.getChildNodes();
+        return secondNodeList.item(0).getNodeValue();
+    }
+
+    private String[][] prepareNastedeArr(){
+        String urlAddress = "http://api.nbp.pl/api/exchangerates/tables/a/?format=xml";
+        String content = getUrlContent(urlAddress);
+
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        String[] tagNames ;
+        String[][] arr = null;
+
+
+        try
+        {
+
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            // w tym miejscu konertowanie zawatości strony na plik XML
+            Document doc = db.parse(new InputSource(new StringReader(content)));
+
+            NodeList list = doc.getElementsByTagName("Rate");
+            NodeList nl = list.item(0).getChildNodes();
+            tagNames = new String[nl.getLength()];
+            for(int i = 0; i < tagNames.length; i++)
+            {
+                tagNames[i] = nl.item(i).getNodeName();
+            }
+            arr = new String[list.getLength()][3];
+
+            for(int i = 0; i < list.getLength(); i++)
+            {
+                Node node = list.item(i);
+                if(node.getNodeType()==Node.ELEMENT_NODE)
+                {
+                    for(int j = 0; j < tagNames.length; j++)
+                    {
+                        arr[i][j] = getNode(node,tagNames[j]);
+                    }
+                }
+            }
+            return arr;
+        }catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return arr;
+    }
+
+    private void initializeCourses(){
+        String[][] coursesArr = prepareNastedeArr();
+
+        for(int i = 0; i < coursesArr.length; i++){
+            for(int j = 0; j < coursesArr[i].length; j++){
+                String[] tmpArr = new String[coursesArr[i].length-1];
+                String valuteName = null;
+                if(j == 0)
+                    valuteName = coursesArr[i][j];
+                else
+                    tmpArr[j-1] = coursesArr[i][j];
+
+                if(j == coursesArr[i].length - 1)
+                    courses.put(valuteName,tmpArr);
+            }
+        }
+    }
+
+    public String[] getValute(String valute){
+        String[] value = null;
+
+        value = courses.get(valute);
+
+        return value;
+    }
+}
--- src/main/java/servlets/MainServlet.java
+++ src/main/java/servlets/MainServlet.java
@@ -1,17 +1,59 @@
 package servlets;
 
+import org.json.JSONObject;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MainServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
         response.setContentType("text/html");
 
-        RequestDispatcher dispetcher = request.getRequestDispatcher("index.html");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.html");
 
+        if(dispatcher != null){
+            dispatcher.forward(request, response);
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+        StringBuilder builder = new StringBuilder();
+        String line = null;
+
+        try{
+            BufferedReader reader = request.getReader();
+
+            while((line=reader.readLine())!=null){
+                builder.append(line);
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        try{
+
+            JSONObject jsonObject = new JSONObject(builder.toString());
+
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+
+            String command = jsonObject.getString("command");
+
+            switch(command){
+                case "vievCfg":
+                    break;
+                case "get":
+                    break;
+            }
+
+        }catch(Exception e){
+            e.getMessage();
+        }
     }
 }
